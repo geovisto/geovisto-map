1887ba957f2965a0d6780bafde5b4e661c8cb303
tkosci <tomas.koscielniak@gmail.com>
2022-04-01 22:52:21 +0200

    Feature #31: Fix interface type in Marker legend

--- src/tools/layers/marker/model/internal/legend/MarkerLayerToolMapLegend.ts
+++ src/tools/layers/marker/model/internal/legend/MarkerLayerToolMapLegend.ts
@@ -27,19 +27,19 @@ class MarkerLayerToolMapLegend extends MapLayerToolLegend<IMarkerLayerTool> impl
     /**
      * It returns the legend.
      */
-    public getContent(tool: IMapTool): HTMLElement  | undefined {
+    public getContent(tool: IMarkerLayerTool): HTMLElement  | undefined {
         const div = document.createElement('div');
         div.className = "legend";
         let category_name: string;
         let value_name: string;
-        const categories = tool.getState().currentDataCategories;
+        const categories = tool.getState().getCurrentDataCategories();
         // Define colors
         const colors = ["#0006a7", "#c1c100", "#c10000"];
         // Get category and value name
-        try {category_name = tool?.getState().dimensions.category.value.name;}
-        catch { category_name = tool?.getState().dimensions.category.domainName;}
-        try {value_name = tool?.getState().dimensions.value.value.name;}
-        catch { value_name = tool?.getState().dimensions.value.domainName;}
+        try {category_name = tool?.getState().getDimensions().category.value.name;}
+        catch { category_name = tool?.getState().getDimensions().category.domainName;}
+        try {value_name = tool?.getState().getDimensions().value.value.name;}
+        catch { value_name = tool?.getState().getDimensions().value.domainName;}
         if (value_name == undefined || value_name == "") {
             // No value entered yet - dont create legend
             return undefined;
