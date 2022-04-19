2ec6f9047c55c2bd061e745bdc8262513545faf8
FabioHexo <vojta.maly7@gmail.com>
2022-04-19 23:32:49 +0200

    Feature #27: Interface change of Geo data manager for hierarchy.

--- src/model/internal/geodata/GeoDataManager.ts
+++ src/model/internal/geodata/GeoDataManager.ts
@@ -1,6 +1,9 @@
+import { FeatureCollection} from "geojson";
+import GeoJSONTypes from "../../types/geodata/GeoJSONTypes";
 import IGeoData from "../../types/geodata/IGeoData";
 import IGeoDataManager from "../../types/geodata/IGeoDataManager";
 import MapDomainArrayManager from "../domain/generic/MapDomainArrayManager";
+import HierarchyTree from "./HierarchyTree";
 
 /**
  * The class provides a basic implemention geographical data manager.
@@ -8,9 +11,105 @@ import MapDomainArrayManager from "../domain/generic/MapDomainArrayManager";
  * @author Jiri Hynek
  */
 class GeoDataManager extends MapDomainArrayManager<IGeoData> implements IGeoDataManager {
-    
     public constructor(geoDataArray: IGeoData[]) {
         super(geoDataArray);
+    }
+
+
+    /*--- Hierarchy interface ---*/
+    private hierarchy = false;                                  
+    private TreeMap : Map<string, HierarchyTree> = new Map();
+
+    public enableHierarchy(enabled : boolean) : void{
+        this.hierarchy = enabled;
+    }
+
+    public isHierarchyEnabled() : boolean {
+        return this.hierarchy;
+    }
+
+    public isHierarchyEnabledForDomain(domainName: string): boolean {
+        return this.TreeMap.has(domainName);
+    }
+
+    public getFeatures(name : string, types : string[]) : FeatureCollection | undefined {
+        const geoData = this.getDomain(name);
+        const list = geoData?.getFeatures(types);
+        let active : string[] = [];
+        const answer: FeatureCollection = {
+            type: GeoJSONTypes.FeatureCollection,
+            features: []
+        };
+        if (this.TreeMap.has(name)) {
+            active = this.TreeMap.get(name)?.getActive() ?? [];
+        }
+        list?.features.forEach(feat => {
+            if (feat.id) {
+                if(active.includes(feat.id.toString())) {
+                    answer.features.push(feat);
+                }
+            }
+        });
+        if (!this.hierarchy) {
+            list?.features.forEach(feat => {
+                answer.features.push(feat);
+            });
+        }
+        return answer;
+    }
+
+    public setTree(domainName : string, nodes : [string, string | boolean, number][], aggregationEnabled : boolean) : void{
+        const newTree = new HierarchyTree(domainName);
+        newTree.initByArray(nodes);
+        newTree.setAggregationFlag(aggregationEnabled);
+        this.TreeMap.set(domainName, newTree);
+        
+    }
+
+    public startTree(domainName : string, zoom : number) : void {
+        if (this.TreeMap.has(domainName)) {
+            this.TreeMap.get(domainName)?.startActiveWatch(zoom);
+        }
+    }
+
+    public updateTrees(zoom : number) : void {
+        this.TreeMap.forEach(tree => {
+            tree.recalculate(zoom);
+        });
+    }
+
+
+    public didSomeTreeChanged() : boolean {
+        let x = false;
+        this.TreeMap.forEach(tr => {
+            if(tr.getChangeFlag()) {
+                x = true;
+            }
+        });
+        return x;
+    }
+
+    public treeAggregationFlag(name : string) : boolean {
+        if (this.TreeMap.has(name)) {
+            return this.TreeMap.get(name)?.getAggregationFlag() ?? false;
+        }
+        return false;
+    }
+
+    public getActiveByTree(name : string) : string[] {
+        let answer : string[] = [];
+        if (this.TreeMap.has(name)) {
+            answer = this.TreeMap.get(name)?.getActive() ?? [];
+        }
+        return answer;
+    }
+
+    public getChildsFromTree(name : string, geoOb : string) : string[] {
+        let answer : string[] = [];
+        if (this.TreeMap.has(name)) {
+            answer = this.TreeMap.get(name)?.getAllLeafes(geoOb) ?? [];
+        }
+        return answer;
     }
 }
 export default GeoDataManager;
--- src/model/types/geodata/IGeoDataManager.ts
+++ src/model/types/geodata/IGeoDataManager.ts
@@ -1,10 +1,77 @@
 import IGeoData from "./IGeoData";
 import IMapDomainArrayManager from "../domain/IMapDomainArrayManager";
+import { FeatureCollection } from "geojson";
 
 /**
  * The type represents geographical data manager.
  * 
  * @author Jiri Hynek
  */
-type IGeoDataManager = IMapDomainArrayManager<IGeoData>;
+type IGeoDataManager = IMapDomainArrayManager<IGeoData> & {
+    isHierarchyEnabled() : boolean;
+
+    /**
+     * Change enable status of hierarchy.
+     */
+    enableHierarchy(enabled:boolean) : void;
+
+    /**
+     * Sets new hierarchy tree.
+     * @param domainName - Name of domain
+     * @param nodes - Array of nodes in forma : [Id of node, Id of parent (if parent it is boolean set to true), level of zoom]
+     * @param aggregationEnabled - Status of agregation, set by configuration file.
+     */
+    setTree(domainName : string, nodes : [string, string | boolean, number][], aggregationEnabled : boolean) : void;
+
+    /**
+     * Overloaded function, returns only active GeoJSON object.
+     * @param name - Name of requested domain
+     * @param types - Points / Polygons / etc..
+     */
+    getFeatures(name : string, types : string[]) : FeatureCollection | undefined 
+    
+    /**
+     * Start's active watch of active zoom levels for tree.
+     * Used to set initial level of zoom. (Map might render from start on different levels, based on configuration.)
+     * @param domainName - Name of domain.
+     * @param zoom - Initial zoom level
+     */
+    startTree(domainName : string, zoom : number) : void;
+    
+    /**
+     * Update active objects of each tree based on new zoom.
+     * @param zoom - New level of zoom.
+     */
+    updateTrees(zoom : number) : void;
+
+    /**
+     * Returns if hierarchy is enabled and all set up for certain tree.
+     * @param domainName - Name of domain.
+     */
+    isHierarchyEnabledForDomain(domainName: string): boolean;
+    
+    /**
+     * Returns true, if active objects of some tree changed after last zoom.
+     */
+    didSomeTreeChanged() : boolean;
+
+    /**
+     * Returns true, if aggregation of data should be performed on certain tree.
+     * @param name - Name of domain
+     */
+    treeAggregationFlag(name : string) : boolean;
+
+    /**
+     * Get active objects of tree.
+     * @param name - Name of domain.
+     */
+    getActiveByTree(name : string) : string[];
+
+    /**
+     * Get child nodes ID's of parent.
+     * @param name - Name of domain
+     * @param geoOb - ID of parent
+     */
+    getChildsFromTree(name : string, geoOb : string) : string[];
+};
 export default IGeoDataManager;
