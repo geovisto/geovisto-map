0d7e58b4a85049da5fbdf3bc1e43b47882cbafc2
Vladimir Korencik <vladimir.korencik@ligasconsulting.com>
2023-03-02 20:54:46 +0100

    Feature #9: Typescript fix

--- src/tools/timeline/model/internal/utils.ts
+++ src/tools/timeline/model/internal/utils.ts
@@ -1,1 +1,2 @@
-export const path = (path: string, obj: Record<string, unknown>): any => path.split('.').reduce((o, i) => o[i], obj);
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const path = (path: string, obj: Record<string, any>): any => path.split('.').reduce((o, i) => o[i], obj);
