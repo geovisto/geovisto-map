005c96d796710c76f579490a187c24b4f275e024
Vladimir Korencik <vladimir.korencik@ligasconsulting.com>
2023-04-01 16:37:43 +0200

    Feature #9: fixed formatDate bug

--- src/tools/timeline/model/internal/utils.ts
+++ src/tools/timeline/model/internal/utils.ts
@@ -19,7 +19,7 @@ export const formatDate = (date: Date, granularity: TimeGranularity): string => 
     const hours = ("0" + date.getHours()).slice(-2); 
     const minutes = ("0" + date.getMinutes()).slice(-2);
 
-    const day = ("0" + (date.getDay() + 1)).slice(-2); 
+    const day = ("0" + date.getDate()).slice(-2); 
     const month = ("0" + (date.getMonth() + 1)).slice(-2); 
     const year = date.getFullYear().toString();
 
